382b29cbb81aa91d295435a929487a93e2251815
Bryan Opfer <bopfer@gmail.cm>
2018-10-03 09:29:54 -0700

    Add Context and more wrappers

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import { Trans, withI18n, withI18nProps } from '@lingui/react';
+import { RouteComponentProps } from '@reach/router';
+import { AppContext } from './AppContext';
 
-interface AppProps extends withI18nProps { }
+interface AppProps extends RouteComponentProps, withI18nProps {
+  appContext: AppContext;
+}
 
 class AppBase extends React.PureComponent<AppProps> {
   public render() {
--- packages/app/src/utils/testing.tsx
+++ packages/app/src/utils/testing.tsx
@@ -1,5 +1,41 @@
 import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
+import * as React from 'react';
+import { render } from 'react-testing-library';
+import { RouteComponentProps, createHistory, createMemorySource, LocationProvider } from '@reach/router';
+import { I18nProvider } from '@lingui/react';
+import { setupI18n } from '@lingui/core';
+import { MockedProvider, MockedResponse } from 'react-apollo/test-utils';
+
+import { AppContextProvider } from '../AppContext';
+
+beforeEach(() => {
+  jest.resetModules();
+});
+
+interface RenderWrapperProps {
+  node: React.ReactElement<RouteComponentProps>;
+  options?: { container: HTMLElement; baseElement?: HTMLElement };
+}
+
+export const renderWrapper = (props: RenderWrapperProps) => {
+  const testHistory = createHistory(createMemorySource('/'));
+  const i18n = setupI18n();
+  const gqlMocks: MockedResponse[] = [];
+
+  return render(
+    <AppContextProvider>
+      <MockedProvider mocks={gqlMocks}>
+        <I18nProvider i18n={i18n} language="en">
+          <LocationProvider history={testHistory}>
+            {props.node}
+          </LocationProvider>
+        </I18nProvider>
+      </MockedProvider>
+    </AppContextProvider>,
+    props.options
+  );
+};
 
 // re-export everything in r-t-l so they can all be imported from this file.
 export * from 'react-testing-library';
